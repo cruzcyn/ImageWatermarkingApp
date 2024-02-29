71503a5974ad49d8c36c5e91812e51e45919300e
cynthiacruz <ccruzgz@gmail.com>
2024-02-29 12:44:41 -0600

    Add the functionality to the add_watermark function to transform the image and display it in the GUI. Delete other functions and buttons as they're unnecessary to the app with this approach

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from tkinter import *
+from PIL import Image, ImageTk, ImageDraw
+from urllib.request import urlopen
 
 # Consider having a global variable for the watermark
 
-# Button functions:
-def upload_img():
-    pass
-
-
-def upload_watermark():
-    pass
-
-
 def add_watermark():
-    pass
+    img_url = img_route.get()
+    user_text = watermark_text.get()
+
+    user_img = Image.open(urlopen(img_url))
+    finished_img = ImageDraw.Draw(user_img)
+    finished_img.text(xy=(0,0), text=user_text, fill="white")
+
+    display_img = ImageTk.PhotoImage(user_img)
+
+    image_label = Label(image=display_img)
+    image_label.image = display_img
+    image_label.grid(row=3, column=0, columnspan=2)
 
 # CREATE INTERFACE
 window = Tk()
@@ -21,17 +25,13 @@ window.minsize(width=500, height=500)
 
 # Upload img
 img_route = Entry(width=30)
+img_route.insert(END, string="URL of your image")
 img_route.grid(row=0, column=0)
 
-img_upload_btn = Button(text="Upload Image", command=upload_img)
-img_upload_btn.grid(row=0, column=1)
-
 # Upload watermark
-watermark_route = Entry(width=30)
-watermark_route.grid(row=1, column=0)
-
-watermark_upload_btn = Button(text="Upload Watermark", command=upload_watermark)
-watermark_upload_btn.grid(row=1, column=1)
+watermark_text = Entry(width=30)
+watermark_text.insert(END, string="Type what you want your watermark to say.")
+watermark_text.grid(row=1, column=0)
 
 # Add watermark to image
 convert_img = Button(text="Add your watermark!", command=add_watermark)
